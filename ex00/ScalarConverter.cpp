14a1bde8db4a271162b1a7a71c0f7cc23068e7e9
Theo POTILLION <tpotilli@made-f0Br5s11.clusters.42paris.fr>
2024-10-24 13:34:03 +0200

    pus

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -24,104 +24,168 @@ ScalarConverter::~ScalarConverter()
 
 void ScalarConverter::convert(std::string str)
 {
+    std::string test = " +inff";
+    std::string tmp = "-inff" + test + " nan" " nanf" + " -inf" + " +inf";
     char c = str[0];
     int it;
     double db;
     float ft;
+    int flag = 0;
+    int check_part = 0;
+    (void)c;
 
-    if (str.find(".") == 0)
+    std::cout << "voici ce que find trouve " << str.find(".") << " ";
+    if (str == "-inff" || str == "+inff" || str == "nan" || str == "nanf" || str == "-inf"
+        || str == "+inf")
+        check_part = 1;
+    if (str.find(".") < str.size())
     {
-        if (str.find("f") == 0)
+        if (str.find("f") < str.size())
+        {
+            flag = 1;
+            std::cout << "float" << std::endl;
             std::istringstream(str) >> ft;
+            db = static_cast<float>(ft);
+            it = static_cast<int>(ft);
+        }
         else
+        {
+            std::cout << "double" << std::endl;
             std::istringstream(str) >> db;
+            ft = static_cast<double>(db);
+            it = static_cast<int>(db);
+        }
     }
-    else if (str.find(""))
+    else if (str[0] >= 48 && str[0] <= 57)
     {
-
+        std::cout << "int" << std::endl;
+        std::istringstream(str) >> it;
+        db = static_cast<float>(it);
+        ft = static_cast<double>(it);
     }
-    db = static_cast<float>(it);
-    ft = static_cast<float>(db);
-}
-
-void ScalarConverter::convert(std::string str)
-{
-    std::string test = " +inff";
-    std::string tmp = "-inff" + test + " nan" " nanf" + " -inf" + " +inf";
-    char cr = str[0];
-    int it;
-    double db;
-    float ft;
-
-    // std::cout << "voici input" << str << std::endl;
-    std::istringstream(str) >> it;
-    std::stringstream(str) >> db;
-    ft = static_cast<float>(db);
+    else
+    {
+        std::cout << "char" << std::endl;
+        c = str[0];
+        it = static_cast<int>(c);
+        db = static_cast<double>(c);
+        ft = static_cast<float>(c);
+    }
     int last = tmp.find(str);
     std::cout << last << std::endl;
 
-    // verification du char
-    int stop = str.find(".");
-    if (str == "-inff" || str == "+inff" || str == "nan" || str == "nanf" || str == "-inf"
-        || str == "+inf")
+    // pour les char
+
+    if (check_part == 1)
     {
-        std::cout << "char : impossible" << std::endl;
+        std::cout << "char: impossible" << std::endl;
     }
-    else if (stop == 1)
-        std::cout << "char: " << '*' << std::endl;
-    else if (str[0] >= 48 && str[0] <= 57)
-        std::cout << "char : Non displayable" << std::endl;
-    else if (std::isprint(cr) != 0)
-        std::cout << "char: " << cr << std::endl;
-    // else if (last == -1 || (str.length() > 1 && str[0] <= 48 && str[0] >= 57))
-        // std::cout << "char: impossible" << std::endl;
 
-    //verification du int
+    // pour les int
 
-    if (last != -1)
-		std::cout << "int: impossible" << std::endl;
-	else
-		std::cout << "int: " << it << std::endl;
-
-    //verif du float
-    // std::cout << "voici last " << last << " et lost " << stop << std::endl;
-    if (last != -1)
-    {
-        if (str == "nan")
-            std::cout << "float: nanf" << std::endl;
-        else if (str == "-inf")
-            std::cout << "float: -inff" << std::endl;
-        else if (str == "+inf")
-            std::cout << "float: +inff" << std::endl;
-        else
-            std::cout << "float: " << str << std::endl;
-    }
+    if (check_part == 1)
+        std::cout << "int: impossible" << std::endl;
     else
-        std::cout << "float: " << ft << ".0f"<< std::endl;
-    // else if (stop != -1)
-    // else
-        // std::cout << "float: " << ft << "f" << std::endl;
+        std::cout << "int: " << it << std::endl;
 
-    //verif du double
-    last = tmp.find(str);
-    // std::cout << "voici last " << last << " et lost " << stop << std::endl;
-    if (last != -1)
-    {
-        if (str == "nanf")
-            std::cout << "double: nan" << std::endl;
-        else if (str == "-inff")
-            std::cout << "double: -inf" << std::endl;
-        else if (str == "+inff")
-            std::cout << "double: +inf" << std::endl;
-        else
-            std::cout << "double: " << str << std::endl;
-    }
+    // pour les float
+
+    if (str == "nan")
+        std::cout << "float: nanf" << std::endl;
+    else if (str == "-inf")
+        std::cout << "float: -inff" << std::endl;
+    else if (str == "+inf")
+        std::cout << "float: +inff" << std::endl;
+    if (flag == 1)
+        std::cout << "float: " << ft << "f" << std::endl;
     else
-        std::cout << "double: " << db << ".0"<< std::endl;
-    // else if (stop == -1)
-        // std::cout << "double: " << db << std::endl;
-    // std::cout << "double: " << db << std::endl;
+        std::cout << "float: " << ft <<  ".0f" << std::endl;
+
+    // pour les double
+    if (str == "nanf")
+        std::cout << "double: nan" << std::endl;
+    else if (str == "-inff")
+        std::cout << "double: -inf" << std::endl;
+    else if (str == "+inff")
+        std::cout << "double: +inf" << std::endl;
+    else
+        std::cout << "double: " << db << std::endl;
 }
+
+// void ScalarConverter::convert(std::string str)
+// {
+//     char cr = str[0];
+//     int it;
+//     double db;
+//     float ft;
+
+//     // std::cout << "voici input" << str << std::endl;
+//     std::istringstream(str) >> it;
+//     std::stringstream(str) >> db;
+//     ft = static_cast<float>(db);
+
+//     // verification du char
+//     int stop = str.find(".");
+//     if (str == "-inff" || str == "+inff" || str == "nan" || str == "nanf" || str == "-inf"
+//         || str == "+inf")
+//     {
+//         std::cout << "char : impossible" << std::endl;
+//     }
+//     else if (stop == 1)
+//         std::cout << "char: " << '*' << std::endl;
+//     else if (str[0] >= 48 && str[0] <= 57)
+//         std::cout << "char : Non displayable" << std::endl;
+//     else if (std::isprint(cr) != 0)
+//         std::cout << "char: " << cr << std::endl;
+//     // else if (last == -1 || (str.length() > 1 && str[0] <= 48 && str[0] >= 57))
+//         // std::cout << "char: impossible" << std::endl;
+
+//     //verification du int
+
+//     if (last != -1)
+// 		std::cout << "int: impossible" << std::endl;
+// 	else
+// 		std::cout << "int: " << it << std::endl;
+
+//     //verif du float
+//     // std::cout << "voici last " << last << " et lost " << stop << std::endl;
+//     if (last != -1)
+//     {
+//         if (str == "nan")
+//             std::cout << "float: nanf" << std::endl;
+//         else if (str == "-inf")
+//             std::cout << "float: -inff" << std::endl;
+//         else if (str == "+inf")
+//             std::cout << "float: +inff" << std::endl;
+//         else
+//             std::cout << "float: " << str << std::endl;
+//     }
+//     else
+//         std::cout << "float: " << ft << ".0f"<< std::endl;
+//     // else if (stop != -1)
+//     // else
+//         // std::cout << "float: " << ft << "f" << std::endl;
+
+//     //verif du double
+//     last = tmp.find(str);
+//     // std::cout << "voici last " << last << " et lost " << stop << std::endl;
+//     if (last != -1)
+//     {
+//         if (str == "nanf")
+//             std::cout << "double: nan" << std::endl;
+//         else if (str == "-inff")
+//             std::cout << "double: -inf" << std::endl;
+//         else if (str == "+inff")
+//             std::cout << "double: +inf" << std::endl;
+//         else
+//             std::cout << "double: " << str << std::endl;
+//     }
+//     else
+//         std::cout << "double: " << db << ".0"<< std::endl;
+//     // else if (stop == -1)
+//         // std::cout << "double: " << db << std::endl;
+//     // std::cout << "double: " << db << std::endl;
+// }
 
 // en gros convertir dans les type, voir lequel est le bon
 // donc trouver method pour savoir quel type est le bon
