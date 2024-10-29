63e8a7e289b594e56a3d654f9af709f05d9d5820
Theo POTILLION <tpotilli@made-f0Br5s11.clusters.42paris.fr>
2024-10-29 08:42:47 +0100

    pus

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -108,10 +108,14 @@ void ScalarConverter::convert(std::string str)
         std::cout << "float: -inff" << std::endl;
     else if (str == "+inf" || str == "+inff")
         std::cout << "float: +inff" << std::endl;
-    else if (flag == 1 && (str.find(".") < str.size() && str.find(".0") > str.size()))
-        std::cout << "float: " << ft << "f" << std::endl;
     else
-        std::cout << "float: " << ft <<  ".0f" << std::endl;
+    {
+        size_t pos = str.find(".");
+        if ((pos + 1 >= str.size() || pos >= str.size()) || (str[pos + 1] == 'f' || str[pos + 1] == '0'))
+            std::cout << "float: " << ft <<  ".0f" << std::endl;
+        else
+        std::cout << "float: " << ft <<  "f" << std::endl;
+    }
 
     // pour les double
     if (str == "nanf" || str == "nan")
@@ -124,7 +128,7 @@ void ScalarConverter::convert(std::string str)
     {
         // if (str.find(".") < str.size() && str.find(".0") > str.size())
         size_t pos = str.find(".");
-        if (pos + 1 > str.size() && str.find(".0"))
+        if ((pos + 1 >= str.size() || pos  >= str.size()) || (str[pos + 1] == 'f' || str[pos + 1] == '0'))
             std::cout << "double: " << db << ".0" << std::endl;
         else
             std::cout << "double: " << db << std::endl;
@@ -137,98 +141,3 @@ void ScalarConverter::convert(std::string str)
     // else
     //     std::cout << "double: " << db << ".0" << std::endl;
 }
-
-// void ScalarConverter::convert(std::string str)
-// {
-//     char cr = str[0];
-//     int it;
-//     double db;
-//     float ft;
-
-//     // std::cout << "voici input" << str << std::endl;
-//     std::istringstream(str) >> it;
-//     std::stringstream(str) >> db;
-//     ft = static_cast<float>(db);
-
-//     // verification du char
-//     int stop = str.find(".");
-//     if (str == "-inff" || str == "+inff" || str == "nan" || str == "nanf" || str == "-inf"
-//         || str == "+inf")
-//     {
-//         std::cout << "char : impossible" << std::endl;
-//     }
-//     else if (stop == 1)
-//         std::cout << "char: " << '*' << std::endl;
-//     else if (str[0] >= 48 && str[0] <= 57)
-//         std::cout << "char : Non displayable" << std::endl;
-//     else if (std::isprint(cr) != 0)
-//         std::cout << "char: " << cr << std::endl;
-//     // else if (last == -1 || (str.length() > 1 && str[0] <= 48 && str[0] >= 57))
-//         // std::cout << "char: impossible" << std::endl;
-
-//     //verification du int
-
-//     if (last != -1)
-// 		std::cout << "int: impossible" << std::endl;
-// 	else
-// 		std::cout << "int: " << it << std::endl;
-
-//     //verif du float
-//     // std::cout << "voici last " << last << " et lost " << stop << std::endl;
-//     if (last != -1)
-//     {
-//         if (str == "nan")
-//             std::cout << "float: nanf" << std::endl;
-//         else if (str == "-inf")
-//             std::cout << "float: -inff" << std::endl;
-//         else if (str == "+inf")
-//             std::cout << "float: +inff" << std::endl;
-//         else
-//             std::cout << "float: " << str << std::endl;
-//     }
-//     else
-//         std::cout << "float: " << ft << ".0f"<< std::endl;
-//     // else if (stop != -1)
-//     // else
-//         // std::cout << "float: " << ft << "f" << std::endl;
-
-//     //verif du double
-//     last = tmp.find(str);
-//     // std::cout << "voici last " << last << " et lost " << stop << std::endl;
-//     if (last != -1)
-//     {
-//         if (str == "nanf")
-//             std::cout << "double: nan" << std::endl;
-//         else if (str == "-inff")
-//             std::cout << "double: -inf" << std::endl;
-//         else if (str == "+inff")
-//             std::cout << "double: +inf" << std::endl;
-//         else
-//             std::cout << "double: " << str << std::endl;
-//     }
-//     else
-//         std::cout << "double: " << db << ".0"<< std::endl;
-//     // else if (stop == -1)
-//         // std::cout << "double: " << db << std::endl;
-//     // std::cout << "double: " << db << std::endl;
-// }
-
-// en gros convertir dans les type, voir lequel est le bon
-// donc trouver method pour savoir quel type est le bon
-// le convertir dans les autres types puis afficher
-
-// pour le float, le bon type finis par un f: 42.0f
-// pour le double, il y a un point mais pas le f
-// pour le int ya rien a part le nombre
-// pour le char il y a autre chose qu'un nombre
-
-//stoi -> int
-//stod -> double
-//atof -> float
-
-// -inff
-// +inff
-// nanf(not a number)
-// inf
-// +inf
-// nan
