55ab093922e38638a69782545d3ef7ad5369fe0b
Theo POTILLION <tpotilli@made-f0Br5s11.clusters.42paris.fr>
2024-10-30 12:56:33 +0100

    pus

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -34,6 +34,16 @@ void ScalarConverter::convert(std::string str)
     int check_part = 0;
     (void)c;
 
+    if (str.length() == 1 && !isdigit(str[0]))
+    {
+        // gerer l'affichage.
+        std::cout << "char: " << str[0] << std::endl;
+        std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
+        std::cout << "float: " << static_cast<float>(str[0]) << ".0f" << std::endl;
+        std::cout << "double: " << static_cast<double>(str[0]) << ".0" << std::endl;
+        return ;
+    }
+    
     if (str == "-inff" || str == "+inff" || str == "nan" || str == "nanf" || str == "-inf"
         || str == "+inf")
         check_part = 1;
@@ -48,26 +58,28 @@ void ScalarConverter::convert(std::string str)
         {
             flag = 1;
             std::istringstream(str) >> ft;
-            db = static_cast<float>(ft);
+            db = static_cast<double>(ft);
             it = static_cast<int>(ft);
         }
         else
         {
             std::istringstream(str) >> db;
-            ft = static_cast<double>(db);
+            ft = static_cast<float>(db);
             it = static_cast<int>(db);
         }
-        c = str[0];
+        c = static_cast<int>(it);
     }
     else if ((str[0] >= 48 && str[0] <= 57) || (str[0] >= 48 && str[0] <= 57) || str[0] == '-')
     {
+        std::cout << "je rentre dans int ";
         std::istringstream(str) >> it;
-        db = static_cast<float>(it);
-        ft = static_cast<double>(it);
-        c = str[0];
+        db = static_cast<double>(it);
+        ft = static_cast<float>(it);
+        c = static_cast<int>(it);
     }
     else
     {
+        std::cout << "fils de pute";
         c = str[0];
         it = static_cast<int>(c);
         db = static_cast<double>(c);
@@ -76,22 +88,17 @@ void ScalarConverter::convert(std::string str)
     int last = tmp.find(str);
     std::cout << last << std::endl;
 
-    int sb = atoi(str.c_str());
 
 // regler le char
 // 1.0
-
-    if (check_part == 1 || (str[0] <= 0 || str[0] >= 127))
-        std::cout << "char: impossible" << std::endl;
-    else if ((str[0] <= 0 || str[0] >= 127) && !isprint(sb))
-        std::cout << "char: Non displayable" << std::endl;
-    else if (str.size() < 2 && !isdigit(c) && !isalpha(str[0]))
-        std::cout << "char: " << static_cast<int>(c) << std::endl;
-    else if (isdigit(c))
-        std::cout << "char: " << static_cast<char>(atoi((str.c_str()))) << std::endl;
-    else
-        std::cout << "char: " << str << std::endl;
     // pour les char
+    c = atoi(str.c_str());
+    if (c < 0 || c > 127 || check_part == 1)
+        std::cout << "char: impossible" << std::endl;
+    else if (!isprint(c))
+        std::cout << "char: Non displayable" << std::endl;
+    else
+        std::cout << "char: " << static_cast<char>(c) << std::endl;
 
     // pour les int
 
@@ -104,23 +111,49 @@ void ScalarConverter::convert(std::string str)
 
     size_t dot = 0, f = 0;
     dot = str.find(".");
-    std::cout << "str + 1 " << str[dot + 1] << std::endl;
-    if (dot < str.size())
-    {
-        if ((str[dot + 1] > 48 && str[dot + 1] <= 57) && dot + 2 < str.size())
-            f = 1;
-    }
-    std::cout << "voici f " << f << " ";
+    f = str.find('f');
+    // std::cout << dot << " dot puis size " << str.size() << std::endl;
+    std::cout << std::endl;
+    // std::cout << "str + 1 " << str[dot + 1] << std::endl;
+    // std::cout << "voici f " << f << " ";
     if (str == "nan" || str == "nanf")
         std::cout << "float: nanf" << std::endl;
     else if (str == "-inf" || str == "-inff")
         std::cout << "float: -inff" << std::endl;
     else if (str == "+inf" || str == "+inff")
         std::cout << "float: +inff" << std::endl;
-    else if (f == 1)
-        std::cout << "float: " << ft <<  "f" << std::endl;
+    if (f < str.size())
+    {
+        std::cout << "je passe par tous les f" << std::endl;
+        if (dot >= str.size()) //45.0f
+            std::cout << "float: " << ft <<  ".0f" << std::endl;
+        else if (str[f - 1] == '0' && f - 2 == dot) // 45f
+            std::cout << "float: " << ft <<  ".0f" << std::endl;
+        else if (dot + 1 != '0') // 45.45f
+            std::cout << "float: " << ft <<  "f" << std::endl;
+        // tous les f
+    }
     else
-        std::cout << "float: " << ft <<  ".0f" << std::endl;
+    {
+        std::cout << "je passe par le reste " << std::endl;// 45
+        if (f >= str.size() && dot >= str.size())
+            std::cout << "float: " << ft <<  ".0f" << std::endl;
+        else if (str[dot + 1] == '0' && dot + 2 == str.size()) // 45.0
+            std::cout << "float: " << ft <<  ".0f" << std::endl;
+        else if (dot + 2 < str.size() && (str[dot + 2] != '0' && str[dot + 2] != 'f')) // 45.04
+            std::cout << "float: " << ft <<  "f" << std::endl;
+        // le reste
+    }
+    // 45.0
+    // 45.04
+    // 45
+    // 45.04f
+    // 45f
+
+    // else if (f == 1)
+    //     std::cout << "float: " << ft <<  "f" << std::endl;
+    // else
+    //     std::cout << "float: " << ft <<  ".0f" << std::endl;
     // else
     // {
         // size_t pos = str.find(".");
